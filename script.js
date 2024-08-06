3cce0ee469373182fcecfa641f62335ac8c70e29
Ahmad Sharkawi <ahmadsharkawi59@gmail.com>
2024-08-06 04:10:24 +0300

    v1

---
+++ script.js
@@ -0,0 +1,52 @@
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let currentIndex = 0;
+const images = document.querySelectorAll('#slider figure img');
+const totalImages = images.length;
+
+function updateSlider() {
+  const offset = -currentIndex * 100;
+  document.querySelector('#slider figure').style.left = offset + '%';
+}
+
+function moveLeft() {
+  currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalImages - 1;
+  updateSlider();
+}
+
+function moveRight() {
+  currentIndex = (currentIndex < totalImages - 1) ? currentIndex + 1 : 0;
+  updateSlider();
+}
+
+
+
+
+
+
+
+
+
+
+
